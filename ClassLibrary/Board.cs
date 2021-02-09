0965adca1a7d9c8c6a2ffb80988d6d87a6378e80
Jason C <48025600+Deathblade226@users.noreply.github.com>
2021-02-08 20:27:37 -0700

    Finished Feature 960

--- ClassLibrary/Board.cs
+++ ClassLibrary/Board.cs
@@ -8,6 +8,7 @@
 
 using System;
 using System.Collections;
+using System.Diagnostics;
 using System.Xml;
 
 namespace ChessLibrary
@@ -43,29 +44,100 @@ namespace ChessLibrary
 					m_cells.Add(new Cell(row,col));	// Initialize and add the new chess cell
 				}
 
-			// Now setup the board for black side
-			m_cells["a1"].piece = new Piece(Piece.PieceType.Rook,m_BlackSide);
-			m_cells["h1"].piece = new Piece(Piece.PieceType.Rook,m_BlackSide);
-			m_cells["b1"].piece = new Piece(Piece.PieceType.Knight,m_BlackSide);
-			m_cells["g1"].piece = new Piece(Piece.PieceType.Knight,m_BlackSide);
-			m_cells["c1"].piece = new Piece(Piece.PieceType.Bishop,m_BlackSide);
-			m_cells["f1"].piece = new Piece(Piece.PieceType.Bishop,m_BlackSide);
-			m_cells["e1"].piece = new Piece(Piece.PieceType.King,m_BlackSide);
-			m_cells["d1"].piece = new Piece(Piece.PieceType.Queen,m_BlackSide);
+			randomPlace();
+
 			for (int col=1; col<=8; col++)
 				m_cells[2, col].piece = new Piece(Piece.PieceType.Pawn,m_BlackSide);
 
-			// Now setup the board for white side
-			m_cells["a8"].piece = new Piece(Piece.PieceType.Rook,m_WhiteSide);
-			m_cells["h8"].piece = new Piece(Piece.PieceType.Rook,m_WhiteSide);
-			m_cells["b8"].piece = new Piece(Piece.PieceType.Knight,m_WhiteSide);
-			m_cells["g8"].piece = new Piece(Piece.PieceType.Knight,m_WhiteSide);
-			m_cells["c8"].piece = new Piece(Piece.PieceType.Bishop,m_WhiteSide);
-			m_cells["f8"].piece = new Piece(Piece.PieceType.Bishop,m_WhiteSide);
-			m_cells["e8"].piece = new Piece(Piece.PieceType.King,m_WhiteSide);
-			m_cells["d8"].piece = new Piece(Piece.PieceType.Queen,m_WhiteSide);
+			
 			for (int col=1; col<=8; col++)
 				m_cells[7, col].piece = new Piece(Piece.PieceType.Pawn,m_WhiteSide);
+		}
+
+		public void randomPlace() { 
+		#region Bishop 1
+		Random rng = new Random();
+		int spot1 = rng.Next(0,4) * 2;
+		char b1BoardCell = (char)('a' + spot1);
+		m_cells[b1BoardCell+"1"].piece = new Piece(Piece.PieceType.Bishop, m_BlackSide);
+		m_cells[b1BoardCell+"8"].piece = new Piece(Piece.PieceType.Bishop,m_WhiteSide);
+		#endregion
+
+		#region Bishop 2
+		int spot2 = rng.Next(0,4) * 2;
+		char b2BoardCell = (char)('b' + spot2);
+		m_cells[b2BoardCell + "1"].piece = new Piece(Piece.PieceType.Bishop, m_BlackSide);
+		m_cells[b2BoardCell + "8"].piece = new Piece(Piece.PieceType.Bishop,m_WhiteSide);
+		#endregion
+		
+		#region Queen
+		bool validQ = false;
+		char qBoardCell;
+		do { 
+		int spot3 = rng.Next(0,8);
+		qBoardCell = (char)('a' + spot3);
+		validQ = (qBoardCell != b1BoardCell && qBoardCell != b2BoardCell);
+		} while(!validQ);
+		m_cells[qBoardCell + "1"].piece = new Piece(Piece.PieceType.Queen, m_BlackSide);
+		m_cells[qBoardCell + "8"].piece = new Piece(Piece.PieceType.Queen,m_WhiteSide);
+		#endregion
+
+		#region Knight 1
+		bool validK1 = false;
+		char k1BoardCell;
+		do { 
+		int spot4 = rng.Next(0,8);
+		k1BoardCell = (char)('a' + spot4);
+		validK1 = (k1BoardCell != b1BoardCell && k1BoardCell != b2BoardCell && k1BoardCell != qBoardCell);
+		} while(!validK1);
+		
+		m_cells[k1BoardCell + "1"].piece = new Piece(Piece.PieceType.Knight,m_BlackSide);
+		m_cells[k1BoardCell + "8"].piece = new Piece(Piece.PieceType.Knight, m_WhiteSide);
+		#endregion
+	
+		#region Knight 2
+		bool validK2 = false;
+		char k2BoardCell;
+		do { 
+		int spot5 = rng.Next(0,8);
+		k2BoardCell = (char)('a' + spot5);
+		validK2 = (k2BoardCell != b1BoardCell && k2BoardCell != b2BoardCell && k2BoardCell != qBoardCell && k2BoardCell != k1BoardCell);
+		} while(!validK2);
+		
+		m_cells[k2BoardCell + "1"].piece = new Piece(Piece.PieceType.Knight,m_BlackSide);
+		m_cells[k2BoardCell + "8"].piece = new Piece(Piece.PieceType.Knight, m_WhiteSide);
+		#endregion
+
+		#region King/Rook
+		int spot6 = 0;
+		for (int i = 0; i < 8; i++) {
+		char BoardCell = (char)('a' + i);
+		//Debug.Write(b1BoardCell + "|" + b2BoardCell + "|" + qBoardCell + "|" + k1BoardCell + "|" + k2BoardCell + "|||" + BoardCell + "\n");
+		if (BoardCell != b1BoardCell && BoardCell != b2BoardCell && BoardCell != qBoardCell && BoardCell != k1BoardCell && BoardCell != k2BoardCell) { 
+		switch(spot6) {
+		case 0: 
+		m_cells[BoardCell + "1"].piece = new Piece(Piece.PieceType.Rook, m_BlackSide);
+		m_cells[BoardCell + "8"].piece = new Piece(Piece.PieceType.Rook, m_WhiteSide);
+		spot6++;
+		break;
+		case 1: 
+		m_cells[BoardCell + "1"].piece = new Piece(Piece.PieceType.King, m_BlackSide);
+		m_cells[BoardCell + "8"].piece = new Piece(Piece.PieceType.King, m_WhiteSide);
+		spot6++;
+		break;
+		case 2: 
+		m_cells[BoardCell + "1"].piece = new Piece(Piece.PieceType.Rook, m_BlackSide);
+		m_cells[BoardCell + "8"].piece = new Piece(Piece.PieceType.Rook, m_WhiteSide);
+		spot6++;
+		return;
+		default: Debug.Write("Error: out of range"); break;
+		#endregion
+		}
+
+		}
+
+		}
+
 		}
 
 		// get the new item by rew and column
